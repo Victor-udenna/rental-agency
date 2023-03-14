dd047664029e257c15054057b89cc3a436db08af
Victor-udenna <victorudenna@gmail.com>
2023-03-14 14:45:31 +0100

    Edited lazy loading

--- src/Components/Lazyloading/Lazyloading.js
+++ src/Components/Lazyloading/Lazyloading.js
@@ -4,8 +4,7 @@ import loadingIMG from "../../assets/images/load.png"
 export const Lazyloading = () => {
   return (
 <Fragment>
-<section className=" px-10 property_container py-10 grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 lg:px-20">
-<div className=" pr-0  bg-slate-50 card_property w-[300px] h-[400px] rounded-lg flex flex-col justify-between mb-5 lg:mb-7 justify-between xl:w-[300px]">
+<div className=" pr-0  bg-slate-50 card_property w-[280px] h-[400px] rounded-lg flex flex-col justify-between mb-5 lg:mb-7 justify-between xl:w-[300px]">
     <div className="h-[60%] flex flex-cols items-center justify-center lazyColor rounded-t-lg">
        <img  src={loadingIMG} className='reload' alt=''/>
     </div>
@@ -130,7 +129,6 @@ export const Lazyloading = () => {
 </div>
 </div>
 </div>
-</section>
 </Fragment>
   )
 }
